8b2094dcfe6f2376eedc9d5fba38aa0c40d18627
RakinderJhajj <32330011+RakinderJhajj@users.noreply.github.com>
2020-03-22 17:13:43 +0530

    Update Constant.swift

--- Geo Tag Camera/Geo Tag Camera/Model/Constant.swift
+++ Geo Tag Camera/Geo Tag Camera/Model/Constant.swift
@@ -13,12 +13,14 @@ class Constant {
     struct Color {
         static let kGreenColor   = UIColor(red: 89.0/255.0, green: 223.0/255.0, blue: 44/255.0, alpha: 1.0)
     }
+
     struct String {
-        static let kCamera = "Take Photo"
+         static let KCamera = "Take Photo"
         static let KGallery = "Select from Gallery"
         static let kCameraError = "Sorry, You don't have camera"
         static let kCancel = "Cancel"
-        static let kAppName = "Geo Tag Camera"
+        static let kDone = "Done"
+        static let KAppName = "Geo Tag Camera"
         static let kError = "Error!"
         static let kSuccess = "Success!"
         static let kSaveImageMessage = "Photo has been successfully saved in your Album Geo Tag Camera"
